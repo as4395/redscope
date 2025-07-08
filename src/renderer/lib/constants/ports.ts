8d4be37ca7d98ff11af6d105c23ebcc9c03b1a00
Abhiram <157721594+as4395@users.noreply.github.com>
2025-07-08 17:31:06 -0400

    Create ports.ts

---
+++ src/renderer/lib/constants/ports.ts
@@ -0,0 +1,6 @@
+export const COMMON_PORTS: number[] = [
+  21, 22, 23, 25, 53, 80, 110, 139, 143, 161,
+  443, 445, 465, 993, 995, 1433, 1521, 3306, 3389, 5432,
+];
+
+export const COMMON_PORTS_STRING = COMMON_PORTS.join(',');
